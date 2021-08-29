d25231106d8289d4935520351aa58fc7c871a346
sgonzalezlonzieme <sgonzalezlonzieme@gmail.com>
2021-08-29 16:25:18 -0300

    PI

--- api/src/routes/videogames.js
+++ api/src/routes/videogames.js
@@ -8,10 +8,9 @@ const router = Router();
 router.get('/', async (req, res, next) => {
        
         const {name} = req.query
-
     try{
+        if(name){
 
-      if(name){
         const resultDb = await Videogame.findAll({
             where: {
                  name: {
@@ -20,21 +19,60 @@ router.get('/', async (req, res, next) => {
                 },
             include: Genre,
          })
-         
-        let {id, name, rating, genres, image} = resultDb;
 
-        const responseDb = {
-          id,
-          name,
-          rating,
-          genres: genres.map(p => p.name), 
-          image,
+        const responseDb = resultDb.map(p => 
+            ({
+              id: p.id,
+              name: p.name,
+              image: p.image,
+              genres: p.genres.map(p => p.name),
+              rating: p.rating,
+            }))
+
+        const resultApi = (await axios.get(`https://api.rawg.io/api/games?search=${name}&key=d04c382f770042639c5fecc27e0fd04f`)).data.results
+        
+        const responseApi = resultApi.map(p =>
+            ({
+              id: p.id,
+              name: p.name,
+              image: p.background_image,
+              genres: p.genres.map(p => p.name),
+              rating: p.rating
+            }))
+        
+        const responseDb_Api = [...responseDb, ...responseApi].slice(0, 15)
+          
+        if(responseAll.length){  
+         return res.send(responseDb_Api);
+        }else{
+         return res.send('No Videogames Found')
         }
-
-         return res.send(responseDb)
       }
+        
+       const videogamesDb = await Videogame.findAll({include:Genre})
+
+       const videogamesDbMap = videogamesDb.map(p => ({
+           id: p.id,
+           name: p.name,
+           image: p.image,
+           genres: p.genres.map(p => p.name),
+           rating: p.rating,
+       }))
+
+       const videogamesApi = (await axios.get(`https://api.rawg.io/api/games?key=d04c382f770042639c5fecc27e0fd04f&page_size=200`)).data.results
+
+       const videogamesApiMap = videogamesApi.map(p => ({
+        id: p.id,
+        name: p.name,
+        image: p.background_image,
+        genres: p.genres.map(p => p.name),
+        rating: p.rating,
+    }))
+       
+       return res.send([...videogamesDbMap, ...videogamesApiMap])
 
     }catch(error){
+
         next(error)
     }
     
